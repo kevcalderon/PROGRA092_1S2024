eedc3f05e411acc674d66591195dc885b18a92cb
Kevin Calderón <josue_29c@hotmail.com>
2024-02-24 10:13:45 -0600

    [ADD] clase de ciclos

---
+++ clase5/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.listBoxMostrar = New System.Windows.Forms.ListBox()
+        Me.buttonEjecutar = New System.Windows.Forms.Button()
+        Me.comboBoxDatos = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'listBoxMostrar
+        '
+        Me.listBoxMostrar.FormattingEnabled = True
+        Me.listBoxMostrar.ItemHeight = 20
+        Me.listBoxMostrar.Location = New System.Drawing.Point(41, 113)
+        Me.listBoxMostrar.Name = "listBoxMostrar"
+        Me.listBoxMostrar.Size = New System.Drawing.Size(321, 264)
+        Me.listBoxMostrar.TabIndex = 0
+        '
+        'buttonEjecutar
+        '
+        Me.buttonEjecutar.Font = New System.Drawing.Font("Stencil", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.buttonEjecutar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.buttonEjecutar.Location = New System.Drawing.Point(293, 41)
+        Me.buttonEjecutar.Name = "buttonEjecutar"
+        Me.buttonEjecutar.Size = New System.Drawing.Size(178, 47)
+        Me.buttonEjecutar.TabIndex = 1
+        Me.buttonEjecutar.Text = "Ejecutar Ciclo"
+        Me.buttonEjecutar.UseVisualStyleBackColor = True
+        '
+        'comboBoxDatos
+        '
+        Me.comboBoxDatos.FormattingEnabled = True
+        Me.comboBoxDatos.Location = New System.Drawing.Point(395, 117)
+        Me.comboBoxDatos.Name = "comboBoxDatos"
+        Me.comboBoxDatos.Size = New System.Drawing.Size(317, 28)
+        Me.comboBoxDatos.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.comboBoxDatos)
+        Me.Controls.Add(Me.buttonEjecutar)
+        Me.Controls.Add(Me.listBoxMostrar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents listBoxMostrar As ListBox
+    Friend WithEvents buttonEjecutar As Button
+    Friend WithEvents comboBoxDatos As ComboBox
+End Class
---
+++ clase5/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private Sub buttonEjecutar_Click(sender As Object, e As EventArgs) Handles buttonEjecutar.Click
+        'FOR
+        ''Dim i As Integer = 0
+        Dim totalRepeticiones As Integer = 10
+
+        'EJEMPLO1 - Incremental
+        'For i = 1 To totalRepeticiones
+        '    listBoxMostrar.Items.Add($"Esta linea ha sido repetida {i} veces")
+        'Next
+
+        'EJEMPLO2 - Decrement
+        'For x As Integer = 25 To 0 Step -5
+        '    listBoxMostrar.Items.Add($"El valor actual de X es de {x}")
+        'Next
+
+
+        'EJEMPLO3 - DO WHILE
+
+        'Dim suma As Integer = 0
+        'Dim contador As Integer = 0
+
+        'Do While suma < 100
+        '    '1era. suma = 0 + 20 = 20
+        '    '2do. suma = 20 +20 = 40
+
+        '    suma = suma + 20
+        '    '1era. contador = 0 + 1 = 1
+        '    '1era. contador = 1 + 1 = 2
+
+        '    contador = contador + 1
+        '    listBoxMostrar.Items.Add($"El valor de suma en este ciclo es de: {suma} y el valor de contador es de {contador}")
+        'Loop
+
+
+        'EJEMPLO4 - FIGURA
+        '*
+        '**
+        '***
+        '****
+        ''*****
+        'Dim filas As Integer = 5
+
+        'For i As Integer = 1 To filas
+        '    Dim linea As String = ""
+        '    For j As Integer = 1 To i
+        '        linea &= "*"
+        '    Next
+        '    listBoxMostrar.Items.Add(linea)
+        'Next
+
+
+        'EJEMPLO 5 - COMBOBOX
+        'inicializar datos del combox
+        Dim Descuentos As String() = {"Descuentos Premium", "Descuento Basico", "Descuento Medium"}
+
+        'limpiar el combobox antes de agregar elementos nuevos
+        comboBoxDatos.Items.Clear()
+
+        'Utilizar ciclo
+        For Each item As String In Descuentos
+            comboBoxDatos.Items.Add(item)
+        Next
+
+
+
+
+    End Sub
+End Class
