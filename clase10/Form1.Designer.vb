c794005d93d16f03cbce368887faf14e97a2aac0
Kevin Calderón <josue_29c@hotmail.com>
2024-04-20 10:31:07 -0600

    ejemplo practico eliminar

---
+++ clase10/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.eliminar = New System.Windows.Forms.Button()
+        Me.listUsuarios = New System.Windows.Forms.ListBox()
+        Me.conectar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'eliminar
+        '
+        Me.eliminar.Location = New System.Drawing.Point(314, 327)
+        Me.eliminar.Name = "eliminar"
+        Me.eliminar.Size = New System.Drawing.Size(163, 34)
+        Me.eliminar.TabIndex = 0
+        Me.eliminar.Text = "Eliminar"
+        Me.eliminar.UseVisualStyleBackColor = True
+        '
+        'listUsuarios
+        '
+        Me.listUsuarios.FormattingEnabled = True
+        Me.listUsuarios.ItemHeight = 20
+        Me.listUsuarios.Location = New System.Drawing.Point(170, 105)
+        Me.listUsuarios.Name = "listUsuarios"
+        Me.listUsuarios.Size = New System.Drawing.Size(448, 204)
+        Me.listUsuarios.TabIndex = 1
+        '
+        'conectar
+        '
+        Me.conectar.Location = New System.Drawing.Point(31, 388)
+        Me.conectar.Name = "conectar"
+        Me.conectar.Size = New System.Drawing.Size(115, 30)
+        Me.conectar.TabIndex = 2
+        Me.conectar.Text = "Conectar BD"
+        Me.conectar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Symbol", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(262, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(234, 38)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Eliminar Usuario"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.conectar)
+        Me.Controls.Add(Me.listUsuarios)
+        Me.Controls.Add(Me.eliminar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents eliminar As Button
+    Friend WithEvents listUsuarios As ListBox
+    Friend WithEvents conectar As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ clase10/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private Sub eliminar_Click(sender As Object, e As EventArgs) Handles eliminar.Click
+        If listUsuarios.SelectedIndex <> -1 Then
+            Dim elementoSeleccionado As String = listUsuarios.SelectedItem.ToString()
+            '1,NOMBRE,APELLIDO
+            Dim separacion As String() = elementoSeleccionado.Split(","c)
+            '[1],[NOMBRE],[APELLIDO]
+            Dim idUsuario As String = separacion(0)
+
+            'eliminar el usuario en listbox
+            listUsuarios.Items.RemoveAt(listUsuarios.SelectedIndex)
+
+            'eliminar a nivel de bd
+            Dim connectionString As String = "server=localhost;userId=root;password='';database=clase9"
+            Dim consulta As String = "DELETE FROM Usuario WHERE id = @id"
+
+            Using connection As New MySqlConnection(connectionString)
+                connection.Open()
+                Using command As New MySqlCommand(consulta, connection)
+                    command.Parameters.AddWithValue("id", idUsuario)
+                    command.ExecuteNonQuery()
+                End Using
+            End Using
+
+            'update USUARIO
+            'Dim connectionString As String = "server=localhost;userId=root;password='';database=clase9"
+            'Dim consulta As String = "UPDATE Usuario SET nombre=@nombre, apellido=@apellido WHERE id=@id" 
+
+            'Using connection As New MySqlConnection(connectionString)
+            '    connection.Open()
+            '    Using command As New MySqlCommand(consulta, connection)
+            '        command.Parameters.AddWithValue("id", idUsuario)
+            '        Command.Parameters.AddWithValue("nombre", textboxNombre.Text)
+            '        Command.Parameters.AddWithValue("apellido", textboxApellido.Text)
+            '        command.ExecuteNonQuery()
+            '    End Using
+            'End Using
+        End If
+    End Sub
+
+    Private Sub conectar_Click(sender As Object, e As EventArgs) Handles conectar.Click
+        Dim connectionString As String = "server=localhost;userId=root;password='';database=clase9"
+        Dim connection As New MySqlConnection(connectionString)
+        connection.Open()
+
+        Dim tabla As String = "SELECT id, nombre, apellido FROM Usuario"
+        Dim command As New MySqlCommand(tabla, connection)
+        Dim lectura As MySqlDataReader = command.ExecuteReader()
+
+        listUsuarios.Items.Clear()
+
+        While lectura.Read()
+            'GetString -> varchar
+            'GetUInt16/32 -> enteros
+            listUsuarios.Items.Add(lectura.GetUInt32("id").ToString + "," + lectura.GetString("nombre") + "," + lectura.GetString("apellido"))
+        End While
+        connection.Close()
+    End Sub
+End Class
